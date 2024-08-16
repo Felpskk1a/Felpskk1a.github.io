864698333b44d9303b1ad34527312c22ebd64193
Felpskk1a <00001122405856sp@al.educacao.sp.gov.br>
2024-08-15 21:56:58 -0300

    Add files via upload

---
+++ cadastro_livro.js
@@ -0,0 +1,24 @@
+// cadastro_livro.js
+
+document.getElementById('bookForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const isbn = document.getElementById('isbn').value;
+
+    // Verifique se o ISBN não está vazio
+    if (isbn.trim() === '') {
+        alert('Por favor, insira um ISBN.');
+        return;
+    }
+
+    // Limpar código de barras anterior
+    document.getElementById('barcode').innerHTML = '';
+
+    // Gerar código de barras
+    JsBarcode("#barcode", isbn, {
+        format: "CODE128",
+        width: 2,
+        height: 100,
+        displayValue: true
+    });
+});
